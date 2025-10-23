c41a94a6b03f9eb8a5a369b03a25d3c6d4fcb654
Your Name <you@example.com>
2025-10-23 07:44:56 +0300

    ready for host

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <button className="btn">click me</button>
+      <button className="btn">click mesdsdsdsdsdsdsdsds</button>
     </main>
   );
 }
